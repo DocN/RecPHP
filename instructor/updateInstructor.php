26f7038240efab649e9ab96a17eca2498e042426
DrN <031247ryan@gmail.com>
2018-05-09 16:49:26 -0700

    added instructor management scripts

---
+++ instructor/updateInstructor.php
@@ -0,0 +1,47 @@
+<?php    
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
+error_reporting(0);
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+
+if(!isset($request->instructorID)) {
+    die();
+}
+
+//echo $request->encryptionKey;
+//$res_ar = array("foo"=> $_REQUEST['body']);
+$tester = 0;
+$response = '';
+$servername = "drnserver.duckdns.org";
+$username = "DrN";
+$password = "password123!";
+$dbname = "recdatabase";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$instructorID = $request->instructorID;
+$firstname = $request->firstname;
+$lastname = $request->lastname;
+$photourl = $request->photourl;
+
+$sqlSelect = "UPDATE instructors SET firstname='{$firstname}', lastname='{$lastname}', photoURL='{$photourl}' WHERE instructorID='{$instructorID}'";
+$result = $conn->query($sqlSelect);
+if ($result->num_rows > 0) {
+    $data['message'] = "*Username already exists";
+    $data['valid'] = 0;
+    $conn->close();
+    echo json_encode($data);
+    die();
+} else {
+
+}
+$data['valid'] = 1;
+$conn->close();
+echo json_encode($data);
+
+?>
